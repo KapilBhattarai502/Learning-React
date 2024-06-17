6531aabd623254094ee942b4ddb49da51f7ab8ac
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-06-17 12:27:59 +0545

    destructuring

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 
 const Book = (props) => {
+    const {img,author,name} =props.bookDetails;
     console.log(props);
   return (
     <div style={{
-        boxShadow:"box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;"
+        boxShadow:"rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px",
+        color:"black",
+        padding:"1rem",
+        margin:"1rem",
+        borderRadius:"10px",
+       
+        
 
     }}>
-    <img src={props.img} alt={props.name}></img>
-    <h3>{props.name}</h3>
-    <h6>{props.author}</h6>
+    <img style={{
+        height:"40vmin"
+    }} src={img} alt={name}></img>
+    <h3>{name}</h3>
+    <h6>{author}</h6>
     
     </div>
   )
