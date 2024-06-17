4e8c7914765db1de185a4f3374b0a13a087039d5
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-06-17 10:25:35 +0545

    book component

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,13 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
+import Book from "./components/Book";
 
 const App = () => {
   return (
-    <div>
-      <Booklist/>
-    </div>
-  )
-}
-
-const Booklist =()=>{
-  return (
     <>
-  <Book 
-  bookName="The pshycology of Money"
-  authorName="Robert T. Kiyosaki"
-  bookImage="https://m.media-amazon.com/images/I/51JemODRExL.jpg"
-   />
-  <Book
-    bookName="The Housemaid"
-  authorName="FREIDA McFADDEN"
-  bookImage="https://m.media-amazon.com/images/I/81AHTyq2wVL._SY342_.jpg"
-  />
- 
-  </>)
-
-}
-
-const Book=(props)=>{
-  console.log(props);
-  
-  return(
-    <>
-    
-    <img style={{objectFit:"contain"}}
-    width={400}
-    height={400}
-    
-    src={props.bookImage} alt={props.bookImage}></img>
-    <h4 style={{color:'green'}}>{props.bookName}</h4>
-    <h6>{props.authorName}</h6>
+      <Book/>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Book.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Book = () => {
+  return (
+    <div>
+    <img src='https://m.media-amazon.com/images/I/51JemODRExL.jpg' alt='The pshycology of Money'></img>
+    <h3>Dinner Tonight</h3>
+    <h6>Alex Snodgrass</h6>
+    
+    </div>
+  )
+}
+
+export default Book;
