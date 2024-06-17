cede0dd555f470271c65add3d5b01853ea545e1e
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-06-17 12:28:55 +0545

    making it clean

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import Book from './Book'
+import React from "react";
+import Book from "./Book";
 
 const BookList = () => {
+  const book1 = {
+    img: "https://m.media-amazon.com/images/I/51JemODRExL.jpg",
+    author: "Alex Snodgrass",
+    name: "The Psycology of Mind",
+  };
+  const book2 = {
+    img: "https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg",
+    author:"James Clear",
+    name:"Atomic Habits"
+  };
   return (
-    <div>
-        <Book img="https://m.media-amazon.com/images/I/51JemODRExL.jpg" author="Alex Snodgrass" name="The Psycology of Mind" />
-        <Book img="https://m.media-amazon.com/images/I/513Y5o-DYtL.jpg" author="James Clear" name="Atomic Habits" />
-        
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "space-evenly",
+      }}
+    >
+      <Book bookDetails={book1} />
+      <Book bookDetails={book2} />
     </div>
-  )
-}
+  );
+};
 
-export default BookList
+export default BookList;
