31b5321c798b5ff711c6f5d12703afc0a37ad537
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-06-27 16:31:16 +0545

    learning useEffect

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react';
 
 const Home = () => {
+    const [value,setValue]=useState(0);
+    const [isTen,setIsTen]=useState(false);
+   
+    useEffect(()=>{
+        console.log("Rendering");
+        if(value===10){
+            console.log("The button is clicked 10 times");
+            setIsTen(true);
+        }
+    },[value])
   return (
     <div>
-        <h1 className='homeheading'>Its a Homepage</h1>
+        <h3>Count:{value}</h3>
+        <button onClick={()=>{
+            setValue(value+1);
+        }}>Click Me!</button>
+        {isTen&&<p>You have clicked Ten times</p>}
+
     </div>
   )
 }
