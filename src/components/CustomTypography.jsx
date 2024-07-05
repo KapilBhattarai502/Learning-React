df284a08238abd62ef9e5e8d505ef2dad087a913
Kapil Bhattarai <kapilbhattarai502@gmail.com>
2024-07-05 15:29:49 +0545

    using materail UI

---
+++ src/components/CustomTypography.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Typography} from '@mui/material';
+
+const CustomTypography = () => {
+  return (
+    <div>
+        <Typography variant="h1">Heading H1</Typography>
+        <Typography variant="h2">Heading H2</Typography>
+        <Typography variant="h3">Heading H3</Typography>
+        <Typography variant="h4">Heading H4</Typography>
+        <Typography variant="h5">Heading H5</Typography>
+        <Typography variant="h6">Heading H6</Typography>
+        <Typography variant="subtitle1">It's an subtitle</Typography>
+    </div>
+  )
+}
+
+export default CustomTypography
